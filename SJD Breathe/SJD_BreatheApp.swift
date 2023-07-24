d2cf4978c7c6b0e9f9be8978771ada3384b7a890
Sam Davis <sam@sjd.co>
2023-07-24 11:50:29 +1000

    Initial Commit

---
+++ SJD Breathe/SJD_BreatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  SJD_BreatheApp.swift
+//  SJD Breathe
+//
+//  Created by Samuel Davis on 24/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SJD_BreatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
